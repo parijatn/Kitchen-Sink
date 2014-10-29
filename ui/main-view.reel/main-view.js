9383c36e2aa7e0c7ef233f4884df1ae4fb25bcbd
parijatn <noreply>
2014-10-29 16:07:56 +0000

    Add component MainView

---
+++ ui/main-view.reel/main-view.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/main-view.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MainView
+ * @extends Component
+ */
+exports.MainView = Component.specialize(/** @lends MainView# */ {
+    constructor: {
+        value: function MainView() {
+            this.super();
+        }
+    }
+});
